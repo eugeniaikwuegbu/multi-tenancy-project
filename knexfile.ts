cf9b62c53d7c32fc3b2f7389eb65bfe62c80072b
Eugenia Ikwuegbu <eugenia@zigah.co>
2024-02-21 14:20:14 +0100

    Remove literal values and pass as env variable. Move file location

--- knexfile.ts
+++ knexfile.ts
@@ -1,11 +1,11 @@
 module.exports = {
   client: 'pg',
   connection: {
-    host: 'localhost',
-    user: 'eugenia',
-    password: 'hello1234',
-    database: 'postgres',
-    port: 5432,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
   },
   migrations: {
     directory: './src/database/migrations',
